e716a6f9b654474a38482e1648f587821eed96af
Roland Kuenen <rolandkuenen@gmx.net>
2018-09-21 06:33:36 +0200

    Fehler behoben, dass Anzahl der Buchstaben zurückgesetzt wird, bei einer neuen Eingabe eines Wortes.

--- DasUmfassendeLehrbuch/dulc_a6_1.c
+++ DasUmfassendeLehrbuch/dulc_a6_1.c
@@ -10,10 +10,10 @@ int main()
   char vergleich_buchstabe[2];
   int string_laenge;
   int anzahl_buchstabe;
-  int test_anzahl_buchstaben;
 
   while(1)
   {
+    anzahl_buchstabe = 0;
     printf("Bitte geben Sie ein Wort ein: ");
     fgets(eingabe_wort, 100, stdin);
     printf("Ihre Eingabe: %s\n", eingabe_wort);
@@ -30,17 +30,35 @@ int main()
       vergleich_wort[0] = eingabe_wort[i];
       if(strcmp(vergleich_buchstabe, vergleich_wort) == 0)
       {
+        //Wenn die Buchstaben gleich sind, zähle die Anzahl um eins nach oben.
         //printf("Der Buchstabe %s und %s sind gleich\n", vergleich_buchstabe, vergleich_wort);
         anzahl_buchstabe++;
       }
       else
       {
+        //Wenn die Buchstaben nicht gleich sind, mache nichts.
         //printf("Der Buchstabe %s und %s sind nicht gleich\n", vergleich_buchstabe, vergleich_wort);
       }
     }
     printf("Das Wort %s hat den Buchstaben %c: %d mal \n", eingabe_wort, vergleich_buchstabe[0], anzahl_buchstabe);
-    test_anzahl_buchstaben = check_anzahl_buchstaben(eingabe_wort, strlen(eingabe_wort), eingabe_buchstabe, 2);
-    printf("Das Wort hat den Buchstaben %d Buchstaben.\n", test_anzahl_buchstaben);
   }
   return 0;
 }
+
+/*
+* Aufruf des Programms über: "./FileObject.o lala a"
+*/
+/*
+int laenge_string(char string[]) {
+   int i=0;
+
+   while (string[i] != '\0') i++;
+
+   return i;
+}
+
+int main(int argc, char *arga[]) {
+  int i;
+  printf("%s hat %d Zeichen und soll mit Buchstabe %s durchsucht werden.\n", arga[1], laenge_string(arga[1]), arga[2]);
+ }
+ */
