1d0ae6b3f7d1ce730a01c699ba39c33397fa7b5f
Roland Kuenen <rolandkuenen@gmx.net>
2018-11-28 18:47:02 +0100

    Bubble-Sort in eine Funktion umgewandelt und in HEADER verschoben

--- DasUmfassendeLehrbuch/dulc_a6_7.c
+++ DasUmfassendeLehrbuch/dulc_a6_7.c
@@ -1,30 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "funktionenrk.h"
 
 int main()
 {
+  //int zahlen_array_eingabe[] = {1,3,9,0,4,6,2,8,7,55};
   int zahlen_array_eingabe[10];
   int eingabe_zahl = 0;
-  int temp = 0;
+
   for (int i = 0; i < 10; i++)
   {
     printf("Gib die %d. Zahl ein: ", i+1);
     scanf("%d", &zahlen_array_eingabe[i]);
   }
 
-  for (int i = 1; i < 10; i++)
-  {
-    for (int j = 0; j < 10 - i; j++)
-    {
-      if (zahlen_array_eingabe[j] > zahlen_array_eingabe[j+1])
-      {
-        temp = zahlen_array_eingabe[j];
-        zahlen_array_eingabe[j] = zahlen_array_eingabe[j+1];
-        zahlen_array_eingabe[j+1] = temp;
-      }
-    }
-  }
+  bubblesort(zahlen_array_eingabe, 10);
 
   printf("Sortiert: ");
   for (int i = 0; i < 10; i++)
@@ -32,6 +22,6 @@ int main()
     printf("%d, ", zahlen_array_eingabe[i]);
   }
   printf("\n");
-  
+
   return 0;
 }
