72224392d67909c71a10f2400c0916afcfb45278
Roland Kuenen <rolandkuenen@gmx.net>
2018-12-09 14:10:36 +0100

    Anfang der Aufgabe. Durchlauf des Arrays und füllen der Zahlen

--- DasUmfassendeLehrbuch/dulc_a6_9.c
+++ DasUmfassendeLehrbuch/dulc_a6_9.c
@@ -5,35 +5,52 @@
 int main()
 {
   int *array;
-  int anzahl, index_mitte, spalte, zeile;
+  int groesse, index_mitte, spalte, zeile, dimension;
   int start = 1;
+  int inhalt = 2;
+  int o = 2;
 
   do
   {
     printf("Wählt eine Quadratgröße aus 3, 5, 7, 9: ");
-    scanf("%d", &anzahl);
-  } while((anzahl != 3)&&(anzahl != 5)&&(anzahl != 7)&&(anzahl != 9));
+    scanf("%d", &groesse);
+  } while((groesse != 3)&&(groesse != 5)&&(groesse != 7)&&(groesse != 9));
 
-  array = (int *)malloc(anzahl*sizeof(int));
-  for(int j = 0; j < anzahl; j++)
+  array = (int *)malloc(groesse*sizeof(int));
+  for(int j = 0; j < groesse; j++)
   {
-    for(int k = 0; k < anzahl; k++)
+    for(int k = 0; k < groesse; k++)
     {
-      array[j+anzahl*k] = 0;
+      array[j+groesse*k] = 0;
     }
   }
 
-  index_mitte = anzahl/2;
-  zeile = anzahl/2+1;
-  spalte = anzahl/2;
+  index_mitte = groesse/2;
+  zeile = groesse/2+1;
+  spalte = groesse/2;
+  dimension = groesse*groesse;
 
   // 1. Bedingung
-  array[zeile+anzahl*spalte] = 1;
+  array[zeile+groesse*spalte] = 1;
   //array[index_mitte][index_mitte+1] = 1;
 
-  // 2. Bedingung
+    for (size_t index_zeilen = 0; index_zeilen < groesse; index_zeilen++)
+    {
+      for (size_t index_spalten = 0; index_spalten < groesse; index_spalten++)
+      {
+        printf("Array[%d][%d]: %d\n",index_zeilen, index_spalten, o );
+        array[index_zeilen+groesse*index_spalten] = o;
+      }
+    }
 
-
-  print_array_2d_format((int*)array, anzahl, anzahl);
+  print_array_2d_format((int*)array, groesse, groesse);
+  if(pruefe_magisches_quadrat((int*)array, groesse, groesse, dimension))
+  {
+    printf("Es ist magisch\n");
+  }
+  else
+  {
+    printf("Es ist nicht magisch\n");
+  }
   return 0;
 }
