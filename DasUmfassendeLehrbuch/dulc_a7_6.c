f64ea4f8b62766e15cd7af0fa594bf6ec64defc8
Roland Kuenen <rolandkuenen@gmx.net>
2019-01-04 09:33:14 +0100

    Feste Größe gegen Variable ersetzt

--- DasUmfassendeLehrbuch/dulc_a7_6.c
+++ DasUmfassendeLehrbuch/dulc_a7_6.c
@@ -5,19 +5,20 @@
 
 int main()
 {
-  int zahlen_array_eingabe[] = {1,3,9,0,4,6,2,8,7,55};
+  int zahlen_array_eingabe[] = {8,0,4,1,3,9,100,4,75,77,12,0,7,9,4,6,2,8,7,55,5,77,12,9,0};
+  int lenght = 25;
 
   printf("Unsortiert: ");
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < lenght; i++)
   {
     printf("%d, ", zahlen_array_eingabe[i]);
   }
   printf("\n");
 
-  eigensort(zahlen_array_eingabe, 10);
+  eigensort(zahlen_array_eingabe, lenght);
 
   printf("Sortiert: ");
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < lenght; i++)
   {
     printf("%d, ", zahlen_array_eingabe[i]);
   }
