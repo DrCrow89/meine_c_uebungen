5a0c908d0857159933cfc254d07504b6484cc472
Roland Kuenen <rolandkuenen@gmx.net>
2019-01-06 10:13:02 +0100

    Schachbrettausgabe letzte Zeile hinzugefügt und Start- und Zielfeld einlesen.

--- DasUmfassendeLehrbuch/dulc_a7_9.c
+++ DasUmfassendeLehrbuch/dulc_a7_9.c
@@ -16,12 +16,31 @@ void print_schachbrett(int *array)
     }
     printf("|\n");
   }
+  printf("+---+---+---+---+---+---+---+---+\n");
 }
 
 int main()
 {
   int schachbrett[8][8] = {0};
+  int startfeld_zeile = 9;
+  int startfeld_spalte = 9;
+  int zielfeld_zeile = 9;
+  int zielfeld_spalte = 9;
 
+  while ((startfeld_zeile >= 9)||(startfeld_zeile <= 0)||(startfeld_spalte >= 9)||(startfeld_spalte <= 0))
+  {
+    printf("Bitte geben Sie ein Startfeld (Zeile Spalte) zwischen 1 und 8 an: ");
+    scanf("%d %d", &startfeld_zeile, &startfeld_spalte);
+  }
+
+  while ((zielfeld_zeile >= 9)||(zielfeld_zeile <= 0)||(zielfeld_spalte >= 9)||(zielfeld_spalte <= 0))
+  {
+    printf("Bitte geben Sie ein Zielfeld (Zeile Spalte) zwischen 1 und 8 an: ");
+    scanf("%d %d", &zielfeld_zeile, &zielfeld_spalte);
+  }
+
+  printf("Startfeld: [%d][%d]\n", startfeld_zeile, startfeld_spalte);
+  printf("Zielfeld: [%d][%d]\n", zielfeld_zeile, zielfeld_spalte);
   print_schachbrett((int*)schachbrett);
 
   return 0;
